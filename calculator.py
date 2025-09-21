e4cd0be0128136cabaa800681d8a8728e27dc1e5
Mohammad-Ali-Al-Kammouni <mhmdalikamune@gmail.com>
2025-09-21 21:27:47 +0200

    Creating calculator

---
+++ calculator.py
@@ -0,0 +1,11 @@
+nb1 = 20
+nb2 = 10
+
+print (nb1 , "+", nb2, "=" , nb1+nb2)
+print (nb1 , "-", nb2, "=" , nb1-nb2)
+print (nb1 , "*", nb2, "=" , nb1*nb2)
+print (nb1 , "/", nb2, "=" , nb1/nb2)
+
+name = "Mohammad Ali"
+
+print("This calculator was created by", name)
